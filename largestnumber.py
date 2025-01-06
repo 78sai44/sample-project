cfdc51bb9bc3bf20a96cacbc7000b1423e206a0e
78sai44 <147871306+78sai44@users.noreply.github.com>
2025-01-06 15:56:15 +0530

    Create largestnumber.py

---
+++ largestnumber.py
@@ -0,0 +1,8 @@
+# Program to find the largest number
+numbers = [10, 25, 56, 78, 34, 89, 12]  # Example list of numbers
+
+# Using the max() function to find the largest number
+largest_number = max(numbers)
+
+# Print the result
+print("The largest number is:", largest_number)
