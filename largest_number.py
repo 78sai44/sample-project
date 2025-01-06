9ff90189b389c4c96968629a18d08294c26d7515
78sai44 <147871306+78sai44@users.noreply.github.com>
2025-01-06 17:43:47 +0530

    Update largest_number.py

--- largest_number.py
+++ largest_number.py
@@ -1,3 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    """
+    home fun
+    """
+    return "<h1>Welcome<h1/>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+  
 # Program to find the largest number
 numbers = [10, 25, 56, 78, 34, 89, 12]  # Example list of numbers
 
